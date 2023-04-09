d5807f2b034196acea8ec37f05268c8cd21b3a9b
Vinay <vinay98292jain@gmail.com>
2023-04-09 11:31:47 +0530

    update in top header

--- src/top header/TopHeader.js
+++ src/top header/TopHeader.js
@@ -7,11 +7,18 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import PhoneCallbackIcon from '@mui/icons-material/PhoneCallback';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import PersonIcon from '@mui/icons-material/Person';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function TopHeader() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box >
+      <AppBar position="static" sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <PhoneCallbackIcon /><span>(403) 235-1616</span>
@@ -19,7 +26,16 @@ export default function TopHeader() {
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MailOutlineIcon /><span>orders@printcalgary.com</span>
           </IconButton>
-          
+
+        </Toolbar>
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <PersonIcon /><span>My Account</span>
+          </IconButton>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <FavoriteIcon /><span>Wishlist</span>
+          </IconButton>
+
         </Toolbar>
       </AppBar>
     </Box>
