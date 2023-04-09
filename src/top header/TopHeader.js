b5d7dd18379b5e7b38036a4065931b3af065edf2
Vinay <vinay98292jain@gmail.com>
2023-04-09 11:45:17 +0530

    update top header

--- src/top header/TopHeader.js
+++ src/top header/TopHeader.js
@@ -2,22 +2,34 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import PhoneCallbackIcon from '@mui/icons-material/PhoneCallback';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import PersonIcon from '@mui/icons-material/Person';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { pink } from '@mui/material/colors';
 
 export default function TopHeader() {
   return (
-    <Box >
+    <Box sx={{
+      primary: pink,
+      secondary: {
+        main: '#ffebee',
+        fontSize: '0.5rem',
+      },
+    }}>
       <AppBar position="static" sx={{
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        fontSize: '0.5rem',
+        primary: pink,
+        secondary: {
+          main: '#ffebee',
+        },
       }}>
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
